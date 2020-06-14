ac0397abaa10f77084a95d2e433dd0a400cdba98
cjmash <jamescollins.jcparty@gmail.com>
2020-06-15 02:22:47 +0300

    add procfile

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
   
 const express = require('express');
 const path = require('path');
+const port  = process.env.PORT || 8050
 
 const app = express();
-const publicPath = (path.join(__dirname, '..', 'public'));
-
+const publicPath = (path.join(__dirname, 'public'));
 // Serve only the static files form the dist directory
 app.use(express.static(publicPath));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8050);
+app.listen(() => {
+  console.log('serving on ' + port)
+});
